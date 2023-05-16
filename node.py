f5ada037449a4bb059fc698c1b9fd45426f8d1f9
Qalbun Salim Buanaputra <qalbun.salim@ui.ac.id>
2023-05-16 12:51:55 +0700

    implement leader election

--- node.py
+++ node.py
@@ -49,9 +49,10 @@ class RingNode:
         self.heartbeat_duration = heartbeat_duration
         self.leader = leader
         self.socket = UdpSocket(self.port,self.fault_duration)
+        self.is_participant = False
         index = self.active_nodes.index(self.node_id)
-        self.prev_node = (index + 1) % num_nodes
-        self.next_node = (index - 1) % num_nodes
+        self.prev_node_index = (index - 1) % len(self.active_nodes)
+        self.next_node_index = (index + 1) % len(self.active_nodes)
         
         
     def start_heartbeat_listen(self):
@@ -63,8 +64,86 @@ class RingNode:
                 assert type(message) == dict
                 if message['type'] == "HEARTBEAT":
                     logging.info(f"[HEARTBEAT] Got heartbeat from node_{message['sender_id']}")
+                
+                elif message['type'] == "LEADER_ELECTION":
+                    logging.info(f"[LEADER_ELECTION] Got leader election message from node_{message['sender_id']}")
+                    if self.leader in self.active_nodes:
+                        self.set_leader_as_none()
+
+                    if message['candidate_id'] > self.node_id:
+                        logging.info(f"[LEADER_ELECTION] Candidate id is {message['candidate_id']} which is higher than me. Forwarding to next node...")
+                        message['sender_id'] = self.node_id
+                        self.send_to_next_node(message)
+
+                    elif message['candidate_id'] == self.node_id:
+                        logging.info(f"[LEADER_ELECTION] Message have returned back to me with my id, i am the leader!")
+                        self.leader = self.node_id
+                        raw_msg_sent = {
+                            'sender_id' : self.node_id,
+                            'type': "UPDATE_LEADER",
+                            'leader_id' : self.leader
+                        }
+                        logging.info("[UPDATE_LEADER] Propagating update leader message to next node")
+                        self.send_to_next_node(raw_msg_sent)
+
+                    elif not self.is_participant:
+                        self.is_participant = True
+                        raw_msg_sent = {
+                            'sender_id' : self.node_id,
+                            'type': "LEADER_ELECTION",
+                            'candidate_id' : self.node_id
+                        }
+                        next_node_id = self.active_nodes[self.next_node_index]
+                        logging.info(f"[LEADER_ELECTION] I've voted. Forwarding leader election message to node_{next_node_id}")
+                        self.send_to_next_node(raw_msg_sent)
+
+                    else:
+                        logging.info(f"[LEADER_ELECTION] Already participated in leader election, message discarded")
+
+                elif message['type'] == 'UPDATE_LEADER':
+                    if self.node_id != message['leader_id']:
+                        logging.info(f"[UPDATE_LEADER] Update leader message received. Leader now is node_{message['leader_id']}")
+                        self.leader = message['leader_id']
+                        logging.info(f"[UPDATE_LEADER] Propagating leader message to next node...")
+                        message['sender_id'] = self.node_id
+                        self.send_to_next_node(message)
+                    else:
+                        logging.info(f"[UPDATE_LEADER] Message have come back to me. All active nodes leader are updated!")
             except socket.timeout as e:
-                logging.info("[HEARTBEAT_TIMEOUT] Haven't received heartbeat from previous node")
+                if not self.is_participant:
+                    self.is_participant = True
+                    logging.info("[HEARTBEAT_TIMEOUT] Haven't received heartbeat from previous node")
+                    if self.active_nodes[self.prev_node_index] == self.leader:
+                        logging.info("[LEADER_ELECTION] Leader timeout. Starting leader election...")
+                        self.set_leader_as_none()
+                        raw_msg_sent = {
+                            'sender_id' : self.node_id,
+                            'type': "LEADER_ELECTION",
+                            'candidate_id' : self.node_id
+                        }
+                        next_node_id = self.active_nodes[self.next_node_index]
+                        logging.info(f"[LEADER_ELECTION] I've voted. Forwarding leader election message to node_{next_node_id}")
+                        self.send_to_next_node(raw_msg_sent)
+                    else:
+                        logging.info(f"nih {self.active_nodes[self.prev_node_index]}")
+                        logging.info(f"nih {self.leader}")
+
+    def set_leader_as_none(self):
+        leader_index = self.active_nodes.index(self.leader)
+        self.active_nodes.pop(leader_index)
+        self.active_nodes_ports.pop(leader_index)
+        self.update_prev_and_next_node_index()
+        self.leader = None
+
+    def update_prev_and_next_node_index(self):
+        self.prev_node_index = (self.active_nodes.index(self.node_id) - 1) % len(self.active_nodes)
+        self.next_node_index = (self.active_nodes.index(self.node_id) + 1) % len(self.active_nodes)
+
+    def send_to_next_node(self,raw_msg,offset=0):
+        msg = json.dumps(raw_msg)
+        index = (self.next_node_index + offset) % len(self.active_nodes)
+        dest_port = self.active_nodes_ports[index]
+        self.socket.send(msg,dest_port)
 
     def start_heartbeat(self):
         logging.info("Heartbeat send thread starting...")
@@ -75,13 +154,13 @@ class RingNode:
             next_neighbor_index = (position + 1) % num_nodes
             next_neighbor_id = self.active_nodes[next_neighbor_index]
             next_neighbor_port = self.active_nodes_ports[next_neighbor_index]
-            logging.info(f"Sending to node_{prev_neighbor_id} with port {prev_neighbor_port}")
+            logging.info(f"Sending to node_{next_neighbor_id} with port {next_neighbor_port}")
             raw_message = {
                 'sender_id' : self.node_id,
                 'type' : "HEARTBEAT"
             }
             message = json.dumps(raw_message)
-            self.socket.send(message,prev_neighbor_port)
+            self.socket.send(message,next_neighbor_port)
         
 
 
@@ -114,7 +193,6 @@ def main(heartbeat_duration=1, fault_duration=1, port=1000, active_nodes=[],
     reload_logging_windows(f"logs/node{node_id}.txt")
     threading.excepthook = thread_exception_handler
     try:
-        leader = node_id
         logging.info(f"Node with id {node_id} is running...")
         logging.debug(f"heartbeat_duration: {heartbeat_duration}")
         logging.debug(f"fault_duration: {fault_duration}")
@@ -125,6 +203,7 @@ def main(heartbeat_duration=1, fault_duration=1, port=1000, active_nodes=[],
         node = RingNode(node_id, port, active_nodes, fault_duration, active_nodes_ports, heartbeat_duration, leader)
 
         logging.info("Execute node.start()...")
+        logging.info(f"LEADERNYA {leader}")
         node.start()
     except Exception:
         logging.exception("Caught Error")
